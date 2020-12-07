382b74eec0bb306c774e1c31dbb3b6d596c676e5
madhur1807 <madhurmittal1807@gmail.com>
2020-12-08 00:38:04 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+'use strict';
+(function($) {
+    
+    $('.set-bg').each(function() {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+    
+    
+    var hero_s = $(".hero-items");
+    hero_s.owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: false,
+    });
+    
+    
+})(jQuery);
